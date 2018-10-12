021ced0161f526f18dc89931ff2ec44a4f1c6597
MateuszR <mateusz.rusiecki@amsterdam-standard.pl>
2018-10-12 11:39:20 +0200

    add db, add migration, add simple-node-logger

--- db/migrations/10-10-2018.1.js
+++ db/migrations/10-10-2018.1.js
@@ -3,16 +3,62 @@
 const Promise = require("bluebird");
 const sqlite3 = require("sqlite3");
 const path = require("path");
+let db = new sqlite3.Database('./db/InvoicingApp.db');
 
 module.exports = {
-    up: () => {
-        return new Promise((res, rej) => {
-            let db = new sqlite3.Database('./database/InvoicingApp.db');
-            //enable foreign keys
-            db.run(`PRAGMA foreign_keys = ON`);
-        });
-    },
-    down: () => {
-        
-    }
-}
+  up: () => {
+    return new Promise((res, rej) => {
+      //enable foreign keys
+      db.run(`PRAGMA foreign_keys = ON`);
+      db.serialize(() => {
+        db.run(`CREATE TABLE users(
+                id INTEGER PRIMARY KEY,
+                email TEXT,
+                company_name TEXT,
+                nip TEXT,
+                address TEXT,
+                account_number TEXT,
+                password TEXT
+               )`);
+
+        db.run(`CREATE TABLE invoices(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                client_id INTEGER,
+                type INTEGER,
+                sell_date DATE,
+                issue_date DATE,
+                sum_net INTEGER,
+                sum_vat TEXT,
+                sum_gross INTEGER,
+                paid NUMERIC,
+                FOREIGN KEY(client_id) REFERENCES users(id)
+               )`);
+
+        db.run(`CREATE TABLE transactions(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                price_net INTEGER,
+                value_net INTEGER,
+                value_gross INTEGER,
+                vat TEXT,
+                quantity INTEGER,
+                invoice_id INTEGER,
+                FOREIGN KEY(invoice_id) REFERENCES invoices(id)
+               )`);
+
+        db.close();
+      });
+    });
+  },
+  down: () => {
+    return new Promise((res, rej) => {
+      db.serialize(() => {
+        db.run(`DROP TABLE transactions`);
+        db.run(`DROP TABLE invoices`);
+        db.run(`DROP TABLE users`)
+      });
+      db.close();
+    });
+  }
+};
