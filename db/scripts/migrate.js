14ba74cb8a3548079835c4fb41b5db0d6bd10a2d
MateuszR <mateusz.rusiecki@amsterdam-standard.pl>
2018-10-12 11:40:35 +0200

    add migration running script

---
+++ db/scripts/migrate.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const Umzug = require("umzug");
+
+let umzug = new Umzug({
+  logging: () => {
+    console.log(arguments);
+    //console.dir( arguments );
+  },
+  migrations: {
+    path: "./db/migrations",
+    pattern: /\.js$/
+  },
+  upName: "up",
+  downName: "down"
+});
+
+function logUmzugEvent(eventName) {
+  return (name, migration) => {
+    console.log(`${name} ${eventName}`)
+  };
+}
+
+umzug.on("migrating", logUmzugEvent("migrating"));
+umzug.on("migrated", logUmzugEvent("migrated"));
+umzug.on("reverting", logUmzugEvent("reverting"));
+umzug.on("reverted", logUmzugEvent("reverted"));
+
+umzug.up().then(
+  console.log("All migrations done")
+);
