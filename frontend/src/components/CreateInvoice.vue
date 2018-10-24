22cf1fb7e54c09609e80f79a61a6ab2a2e852837
mateuszr87 <mateuszr87@gmail.com>
2018-10-24 21:56:25 +0200

    Update CreateInvoice component - add transactions list, add methods and data in script tag

--- frontend/src/components/CreateInvoice.vue
+++ frontend/src/components/CreateInvoice.vue
@@ -60,7 +60,160 @@
                         </div>
                     </form>
                 </div>
+
+                <div class="col-md-12">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">Price net</th>
+                                    <th scope="col">VAT</th>
+                                    <th scope="col">Value net</th>
+                                    <th scope="col">Value gross</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <template v-for="txn in transactions">
+                                    <tr :key="txn.id">
+                                        <td>{{ txn.id }}</td>
+                                        <td>{{ txn.name }}</td>
+                                        <td>{{ txn.quantity }}</td>
+                                        <td>{{ txn.price_net }}</td>
+                                        <td>{{ txn.vat }}</td>
+                                        <td>{{ txn.value_net }}</td>
+                                        <td>{{ txn.value_gross }}</td>
+                                        <td><button type="button" class="btn btn-danger" v-on:click="deleteTransaction(txn.id)">X</button></td>
+                                    </tr>
+                                </template>
+                            </tbody>
+                        </table>
+                </div>
+                
+                <div class="form-group">
+                    <button class="btn btn-primary">Create invoice</button>
+                    {{ loading }}
+                    {{ status }}
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'CreateInvoice',
+    data() {
+        return {
+            invoice: {
+                name: "",
+                type: 1,
+                sell_date: new Date().toISOString().slice(0,10),
+                issue_date: new Date().toISOString().slice(0,10),
+                sum_net: 0,
+                sum_vat: 0,
+                sum_gross: 0,
+                paid: 0,
+            },
+            transactions: [],
+            nextTxnId: 1,
+            loading: "",
+            status: "",
+            // invoice: {
+            //     name: "",
+            //     quantity: 0,
+            //     price_net: 0,
+            //     vat: 0,
+            //     value_net: 0,
+            //     value_gross: 0,
+            // },
+        }
+    },
+    methods: {
+        saveTransaction() {
+            this.transactions.push({
+                name: document.getElementById('txn_name_modal').value,
+                quantity: docuemnt.getElementById('txn_quantity_modal').value,
+                price_net: docuemnt.getElementById('txn_price_net_modal').value,
+                vat: docuemnt.getElementById('txn_vat_modal').value,
+                value_net: docuemnt.getElementById('txn_value_net_modal').value,
+                value_gross: docuemnt.getElementById('txn_value_gross_modal').value              
+            });
+
+            this.nextTxnId++;
+            this.calcTotal();
+
+            //clear form fields
+            document.getElementById('txn_name_modal').value = "";
+            docuemnt.getElementById('txn_quantity_modal').value = "";
+            docuemnt.getElementById('txn_price_net_modal').value = "";
+            docuemnt.getElementById('txn_vat_modal').value = "";
+            docuemnt.getElementById('txn_value_net_modal').value = "";
+            docuemnt.getElementById('txn_value_gross_modal').value = ""; 
+        },
+        deleteTransaction(id) {
+            this.transactions = this.transactions.filter(el => {
+                return el.id !== id;
+            });
+            this.nextTxnId--;
+            this.calcTotal();
+        },
+        calcTotal() {
+            let sum_net = 0, sum_vat = 0, sum_gross = 0;
+            this.transactions.forEach(element => {
+                sum_net += element.value_net * 100;
+                sum_vat += element.vat * 100;
+                sum_gross += element.value_gross * 100;
+            });
+
+            this.invoice.sum_net = sum_net;
+            this.invoice.sum_vat = sum_vat;
+            this.invoice.sum_gross = sum_gross;
+        },
+        onSubmit() {
+            let txn_names = [];
+            let txn_quantity = [];
+            let txn_price_net = [];
+            let txn_vat = [];
+            let txn_value_net = [];
+            let txn_value_gross = [];
+
+            this.transactions.forEach(element => {
+                txn_names.push(element.name);
+                txn_quantity.push(element.quantity);
+                txn_price_net.push(element.price_net);
+                txn_vat.push(element.vat);
+                txn_value_net.push(element.value_net);
+                txn_value_gross.push(element.value_gross);
+            });
+
+            let formData = {
+                client_id: this.$route.params.user.id,
+                name: this.invoice.name,
+                type: this.invoice.type,
+                sell_date: this.invoice.sell_date,
+                issue_date: this.invoice.issue_date,
+                sum_net: this.invoice.sum_net,
+                sum_vat: this.invoice.sum_vat,
+                sum_gross: this.invoice.sum_gross,
+                paid: this.invoice.paid,
+                //transactions: transactions,
+                txn_names: txn_names,
+                txn_quantity: txn_quantity,
+                txn_price_net: txn_price_net,
+                txn_vat: txn_vat,
+                txn_value_net: txn_value_net,
+                txn_value_gross: txn_value_gross
+            }; 
+
+            axios.post(env.default.SERVER_ADDR + 'invoice', formData).then(res => {
+                this.loading = "";
+                this.status = res.data.message;
+            });
+        }
+    }
+}
+</script>
